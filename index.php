bdd59fb7c119aca476e865c3d0660c00aedfbf45
SilverClaws02 <72350529764@szily.hu>
2025-03-11 10:49:31 +0100

    d

--- index.php
+++ index.php
@@ -2,4 +2,5 @@
 print("Ez a kezdet");
 print("Ez a fejlag1 fejlesztése");
 print("Ez a fejlag2 fejlesztése");
+print("Ez a fejlag3 fejlesztése");
 print("Ez egy harmadik személy fejlesztési része");
